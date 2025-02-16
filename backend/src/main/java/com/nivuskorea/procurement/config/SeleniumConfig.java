87d8917cbb3abdb9e6d54ef508157dd2b9949832
LogInDev <fhazlt303@gmail.com>
2025-02-16 13:50:26 +0900

    입찰공고 - 물품 - -세부품명 검색결과 출력

--- backend/src/main/java/com/nivuskorea/procurement/config/SeleniumConfig.java
+++ backend/src/main/java/com/nivuskorea/procurement/config/SeleniumConfig.java
@@ -1,6 +1,7 @@
 package com.nivuskorea.procurement.config;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -21,17 +22,23 @@ public class SeleniumConfig {
         try {
             System.out.println("Selenium WebDriver 설정 시작... (Headless: " + headless + ")");
 
-            WebDriverManager.chromedriver().setup(); // 크롬 드라이버 자동 설정
+            WebDriverManager.chromedriver().setup(); // 크롬 드라이버 자동 다운로드 및 설정
             ChromeOptions options = new ChromeOptions();
 
             if (headless) {
-//                options.addArguments("--headless"); // UI 없이 실행
+                options.addArguments("--headless"); // UI 없이 실행
             }
 
+            //gpu 가속 비활성화(일부 환경에서의 오류 방지), 샌드박스 모드 비활성화(일부 환경에서 충돌 방지), 공유 메모리 문제 해결
             options.addArguments("--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage");
 
             WebDriver driver = new ChromeDriver(options);
+            // 요소가 나타날 때까지 3초 지연
             driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+            // 브라우저 크기 설정 - 크기별로 생성 버튼이 다르므로 고정
+//            driver.manage().window().setSize(new Dimension(900, 800));
+            driver.manage().window().setSize(new Dimension(1200, 800));
 
             System.out.println("Selenium WebDriver 설정 완료");
             return driver;
--- backend/src/main/java/com/nivuskorea/procurement/controller/selenium/SeleniumController.java
+++ backend/src/main/java/com/nivuskorea/procurement/controller/selenium/SeleniumController.java
@@ -1,14 +1,13 @@
 package com.nivuskorea.procurement.controller.selenium;
 
 import lombok.RequiredArgsConstructor;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/selenium")
@@ -23,36 +22,98 @@ public class SeleniumController {
             driver.get("https://www.g2b.go.kr/");
             Thread.sleep(2000);
 
-            WebElement listButton = driver.findElement(By.id("mf_wfm_gnb_wfm_gnbBtm_btnMenuOpen"));
+        // [메인] - 1200 x 800
+            // 메뉴 리스트 목록 선택
+            WebElement listButton = driver.findElement(By.id("mf_wfm_gnb_wfm_gnbMenu_btnSitemap"));
             listButton.click();
 
-            WebElement selectList = driver.findElement(By.id("mf_wfm_gnb_wfm_gnbMenu_genDepth1_1_btn_menuLvl1"));
-            Actions actions = new Actions(driver);
-            actions.moveToElement(selectList).perform();
-            Thread.sleep(1000);
-
-            WebElement aTag = driver.findElement(By.id("mf_wfm_gnb_wfm_gnbMenu_genDepth1_1_genDepth2_0_btn_menuLvl2"));
-            WebElement selectOn = aTag.findElement(By.xpath("./following-sibling::input[@type='button']"));
+            // '입찰공고목록' 선택
+            WebElement selectOn = driver.findElement(By.id("mf_wfm_gnb_wfm_gnbMenu_genMenu1_1_genMenu2_0_genMenu3_0_btnMenu3"));
             selectOn.click();
 
-            WebElement selectItem = driver.findElement(By.id("mf_wfm_gnb_wfm_gnbMenu_genDepth1_1_genDepth2_0_genDepth3_0_btn_menuLvl3"));
-            selectItem.click();
-
+        // [입찰공고목록]
+            // '상세조건' 버튼 선택
             WebElement searchButton = driver.findElement(By.id("mf_wfm_container_tacBidPbancLst_contents_tab2_body_btnS0004"));
             WebElement detailButton = searchButton.findElement(By.xpath("./following-sibling::div//input[@type='button' and @value='상세조건']"));
             detailButton.click();
 
-            //
-            WebElement searchInput = driver.findElement(By.id("mf_wfm_container_tacBidPbancLst_contents_tab2_body_bidPbancNm"));
-            searchInput.sendKeys("지방상수도");
+            // '업무구분' - '물품'
+            WebElement allCheckbox = driver.findElement(By.xpath(
+                    "//*[@id='mf_wfm_container_tacBidPbancLst_contents_tab2_body_chkBidPbancSrchTyCd']//input[@type='checkbox' and @title='전체']"
+            ));
+            allCheckbox.click();
             Thread.sleep(2000);
+            WebElement productCheckbox = driver.findElement(By.xpath(
+                    "//*[@id='mf_wfm_container_tacBidPbancLst_contents_tab2_body_chkBidPbancSrchTyCd']//input[@type='checkbox' and @title='물품']"
+            ));
+            productCheckbox.click();
+            Thread.sleep(1000);
+
+            // '참가제한지역' 특정 - '전국(제한없음)' || 인천광역시
+            final String limitRegionTagId = "mf_wfm_container_tacBidPbancLst_contents_tab2_body_sbxOdnLmtLgdngCd";
+            final String selectOption = "전국(제한없음)";
+            setSelectOption(limitRegionTagId, selectOption);
+
+            // '세부품명번호' 특정 - 4111249801(프로세스제어반)
+            final String detailProductTagId = "mf_wfm_container_tacBidPbancLst_contents_tab2_body_dtlsPrnmNo";
+            final String productNumber = "4111249801";
+            setSearchText(detailProductTagId, productNumber);
+
+            // 출력 총 index 100
+            final String setIndexTagId = "mf_wfm_container_tacBidPbancLst_contents_tab2_body_sbxRecordCountPerPage1";
+            final String setIndex = "100";
+            setSelectOption(setIndexTagId, setIndex);
+
+            // 검색
+            WebElement searchInput = driver.findElement(By.id("mf_wfm_container_tacBidPbancLst_contents_tab2_body_bidPbancNm"));
             searchInput.sendKeys(Keys.ENTER);
+            Thread.sleep(2000);
+
+        // 검색 결과
+            WebElement resultTable = driver.findElement(By.id("mf_wfm_container_tacBidPbancLst_contents_tab2_body_gridView1_body_table"));
+            WebElement tbody = resultTable.findElement(By.tagName("tbody"));
+            List<WebElement> rows = tbody.findElements(By.tagName("tr"));
+
+            if (!rows.isEmpty()) {
+                for (WebElement row : rows) {
+
+                }
+                rows.stream().forEach(row -> {
+                    String rowText = row.findElements(By.tagName("td")).get(5).getText();
+
+                    if (rowText.length() > 0) {
+
+                        System.out.println("rowText = " + rowText);
+                    }
+                });
+            }
 
             return "Selenium 실행 완료";
 
         } catch (Exception e) {
             return "Selenium 실행 중 오류 발생: " + e.getMessage();
         }
+    }
+
+    /**
+     * 검색어 검색
+     * @param tagId 검색어 작성 Tag id
+     * @param searchText 검색할 검색어
+     */
+    private void setSearchText(String tagId, String searchText) {
+        WebElement detailProductNumber = driver.findElement(By.id(tagId));
+        detailProductNumber.sendKeys(searchText);
+    }
+
+    /**
+     * select 태그에서 option 선택
+     * @param tagId select Tag id
+     * @param selectOption 선택할 selectOption
+     */
+    private void setSelectOption(String tagId, String selectOption) {
+        WebElement selectElement = driver.findElement(By.id(tagId));
+        Select select = new Select(selectElement);
+        select.selectByVisibleText(selectOption);
     }
 
     @GetMapping("/stop")
