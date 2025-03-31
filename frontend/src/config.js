087d33f026122118d918e60272f5eafe7e8af96c
SaeRom Choi <127198819+LogInDev@users.noreply.github.com>
2025-03-31 11:01:55 +0900

    frontend 코드 통합

--- frontend/src/config.js
+++
@@ -1,3 +0,0 @@
-// export const API_BASE_URL = import.meta.env.API_BASE_URL || "http://localhost:8080";
-const API_BASE_URL = window.APP_CONFIG?.API_BASE_URL || "http://nivusds.iptime.org:9291";
-export { API_BASE_URL };
